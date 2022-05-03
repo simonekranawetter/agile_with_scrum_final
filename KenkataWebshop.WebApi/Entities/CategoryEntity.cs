f145e4e47687b1e9716218a4a54d1d7fbdf60ef7
Gunilla Öbom <gunilla@outlook.com>
2022-05-03 10:02:18 +0200

    Created sqlContext category entity and product entity
    Related Work Items: #27

---
+++ KenkataWebshop.WebApi/Entities/CategoryEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KenkataWebshop.WebApi.Entities
+{
+    public class CategoryEntity
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
