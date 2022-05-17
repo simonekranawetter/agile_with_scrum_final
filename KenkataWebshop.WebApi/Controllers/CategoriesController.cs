64500e166b646f17be99a97be1e71562a468e938
Simone Kranawetter <simonekranawetter@gmail.com>
2022-05-17 10:09:07 +0200

    #67 Create action to add new category
    rename controller to categories for consistency

---
+++ KenkataWebshop.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,31 @@
+﻿using KenkataWebshop.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KenkataWebshop.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ILogger<CategoriesController> _logger;
+        private readonly SqlContext _sqlContext;
+
+        public CategoriesController(ILogger<CategoriesController> logger, SqlContext sqlContext)
+        {
+            _logger = logger;
+            _sqlContext = sqlContext;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CategoryDto>> CreateCategory(CategoryDto categoryDto)
+        {
+            var categoryEntity = categoryDto.MapToEntity();
+
+            _sqlContext.Categories.Add(categoryEntity);
+            await _sqlContext.SaveChangesAsync();
+
+            return CreatedAtAction("GetCategoryById", new { id = categoryEntity.Id }, categoryEntity.MapToDto());
+        }
+    }
+}
